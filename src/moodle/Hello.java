e465ef2f585856f18d4c89c3efe76514ad8ba559
xudongwei08 <843617835@qq.com>
2016-01-24 22:33:40 +0800

    test

--- src/moodle/Hello.java
+++ src/moodle/Hello.java
@@ -1,7 +1,26 @@
 package moodle;
 
-public class Hello {
-  ///
-  
+import java.util.HashMap;
+import java.util.Map;
 
+import org.json.JSONObject;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		//解析json
+		String str = "{\"name\":\"xudongwei\"}";
+		JSONObject json = new JSONObject(str);
+		System.out.println(json.get("name"));
+		
+		//封装生成json
+		Map<String,Object> m = new HashMap<String,Object>();
+		m.put("name", "xudongwei");
+		m.put("age", 23);
+		System.out.println(json.valueToString(m));
+		
+	}
+	
+	
 }
